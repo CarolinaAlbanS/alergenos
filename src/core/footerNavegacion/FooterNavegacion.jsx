34a14f80aabacdaa4d3ea5e7006fb56e39e557f2
i-am-iron-man <fabianvanderstoel12@gmail.com>
2024-02-12 16:57:27 +0100

    mas cambios navBar3

--- src/core/footerNavegacion/FooterNavegacion.jsx
+++ src/core/footerNavegacion/FooterNavegacion.jsx
@@ -1,4 +1,5 @@
-<nav>
+export default function NavBar(){
+    return <nav>
     <ul>
         <li> <img src="https://img.freepik.com/vector-premium/casa-icono-logo-casa_419328-2081.jpg?w=996" alt="Home" /> </li>
         {/* <li> <img src="" alt="Favoritos" /> </li>
@@ -6,3 +7,5 @@
         <li> <img src="" alt="Compartir" /> </li> */}
     </ul>
 </nav>
+}
+
