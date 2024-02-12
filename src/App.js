31e344174e6803d919443b985c90da0a41cb57db
Alba <albadvg@yahoo.es>
2024-02-12 15:52:09 +0100

    rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HeaderBurger from './core/headerBurguer/HeaderBurger';
+import Inicio from './paginas/inicio/Inicio';
+import Escaneo from './paginas/escaneo/Escaneo';
+import Bienvenida from './paginas/bienvenida/Bienvenida';
+import Login from './paginas/login/Login';
+import Registo from './paginas/registro/Registo';
+import Favorito from './paginas/favorito/Favorito';
+import Diario from './paginas/diario/Diario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <HeaderBurger/>
+        <Routes>
+          <Route path='/' element={<Inicio/>}></Route>
+          <Route path='/escaneo' element={<Escaneo/>}></Route>
+          <Route path='/bienvenida' element={<Bienvenida/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/registro' element={<Registo/>}></Route>
+          <Route path='/favoritos' element={<Favorito/>}></Route>
+          <Route path='/diario' element={<Diario/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
