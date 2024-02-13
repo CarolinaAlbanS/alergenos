0f40a5c430c4c8dc7989a9b7e8661ab59f24ba7e
CarolinaAlbanS <carolina.alban.sandoval@gmail.com>
2024-02-13 18:52:58 +0100

    estoy metiendo cosas de formularios

--- src/paginas/registro/Registo.jsx
+++ src/paginas/registro/Registo.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import QuienEres from "../../componentes/quienEres/QuienEres";
+import SelecAlergenos from "../../componentes/selecAlergenos/SelecAlergenos";
+import Emergencia from "../../componentes/emergencia/Emergencia";
 
 const Registo = () => {
   return (
     <div>
       <QuienEres></QuienEres>
+      <SelecAlergenos></SelecAlergenos>
+      <Emergencia></Emergencia>
     </div>
   );
 };
