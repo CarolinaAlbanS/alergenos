efeb7c137bd1805e9180116d192ea4cc00255bfa
CarolinaAlbanS <carolina.alban.sandoval@gmail.com>
2024-02-15 20:01:01 +0100

    hecho el token y el desloguearte

--- src/paginas/logout/Logout.jsx
+++ src/paginas/logout/Logout.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Logout = () => {
-  //   const id = localStorage.getItem("id");
+  const id = localStorage.getItem("id");
+  const token = localStorage.getItem("token");
+  console.log(token);
+  console.log(id);
+  const navigate = useNavigate();
+  const handleLogout = async (id) => {
+    // try {
+    //   const res = await axios.post("http://localhost:3001/users/logout");
+    navigate("/");
+    localStorage.removeItem("id");
+    localStorage.removeItem("token");
+
+    // console.log(res);
+    // } catch (error) {
+    //   console.error("fallo en la llamada", error);
+    // }
+  };
+
   return (
     <div>
       <h2>¿Estas seguro que quieres cerrar sesion?</h2>
       <div>
-        <Link>
-          <button to="/portada">Si</button>
-        </Link>
+        <button onClick={handleLogout}>Si</button>
         <button>No</button>
       </div>
     </div>
