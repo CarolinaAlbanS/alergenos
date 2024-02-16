c92d6b6ff0c8d1fc00a6bb74bb329187fc01d625
Alba <albadvg@yahoo.es>
2024-02-16 17:09:58 +0100

    estilos login

--- src/paginas/login/Login.jsx
+++ src/paginas/login/Login.jsx
@@ -27,43 +27,47 @@ const Login = () => {
   };
   return (
     <div className="login">
+
       <div className="login-portada">
         <img className="login-portada__img" src="/img/image@3x.png" alt="portada login: imagen de comida"/>
       </div>
-      <div>
-        <h2>¡Bienvenido de nuevo!</h2>
-        <p>Por favor, introduce tus datos para continuar,</p>
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email"></label>
-        <input
-          type="email"
-          id="email"
-          {...register("email", { required: true })}
-          // onInput={handleSubmit(onSubmit)}
-          placeholder="Dirección de email"
-        ></input>
-        <label htmlFor="password"></label>
-        <input
-          type="password"
-          id="password"
-          {...register("password", { required: true })}
-          // onInput={handleSubmit(onSubmit)}
-          placeholder="Password"
-        ></input>
-        <Link to="/contrseña">
-          <p>¿OLvidaste tu contraseña?</p>
-        </Link>
-        <button>Entrar</button>
-      </form>
-      <div>
-        <p>¿nuevo en Applergic?</p>
-        <Link to="/registro">
-          <h4>Crea tu cuenta aquí</h4>
-        </Link>
-        <Link to="/inicio">
-          <p>Me registraré en otro momento</p>
-        </Link>
+
+      <div className="login-wrap">
+        <div className="login-wrap-txt">
+          <h2 className="login-wrap-txt__h2">¡Bienvenido de nuevo!</h2>
+          <p>Por favor, introduce tus datos para continuar,</p>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", { required: true })}
+            // onInput={handleSubmit(onSubmit)}
+            placeholder="Dirección de email"
+          ></input>
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            id="password"
+            {...register("password", { required: true })}
+            // onInput={handleSubmit(onSubmit)}
+            placeholder="Password"
+          ></input>
+          <Link to="/contrseña">
+            <p>¿OLvidaste tu contraseña?</p>
+          </Link>
+          <button>Entrar</button>
+        </form>
+        <div>
+          <p>¿nuevo en Applergic?</p>
+          <Link to="/registro">
+            <h4>Crea tu cuenta aquí</h4>
+          </Link>
+          <Link to="/inicio">
+            <p>Me registraré en otro momento</p>
+          </Link>
+        </div>
       </div>
     </div>
   );
