1f4cab08e0acc8ed9049b0757b52d61b8cf193e5
franaznar <fran-aznar@hotmail.com>
2024-02-16 15:46:05 +0100

    arregloportada

--- src/componentes/portada/Portada.jsx
+++ src/componentes/portada/Portada.jsx
@@ -3,10 +3,10 @@ import "./Portada.scss";
 export default function Portada (){
     return (
         <div className="ContenedorPortada">
-            <h1>Applergic</h1>
+            <h1 className="ContenedorPortada__h1">Applergic</h1>
             <p>Mi guía alimentaria</p>
             <Link to={"/bienvenida"}>
-            <img className="imagenPortada" src="./img/logoApplergicFigurasGiro.png" alt="imagen de portada" />
+            <img className="imagenPortada" src="./img/logoApplergicFigurasGirocut.png" alt="imagen de portada" />
             </Link>
         </div>
     )
