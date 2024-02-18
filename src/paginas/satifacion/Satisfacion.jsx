11459eaaf7804c9b13b1d37a8be587b21b94c319
franaznar <fran-aznar@hotmail.com>
2024-02-18 20:00:31 +0100

    job3

--- src/paginas/satifacion/Satisfacion.jsx
+++ src/paginas/satifacion/Satisfacion.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./satisfacion.scss"
+import { Link } from "react-router-dom";
 
 const Satisfacion = () => {
     return (
         <div>
-        <h1 className="gracias">Gracias por Su valoración</h1>
+             <Link to="/inicio" className="inicio">
+      <img  className="indice"src="./img/icons/angle-left_10513349.png" alt="flecha"></img> Volver
+      </Link>
+        <h1 className="gracias">Gracias por su valoración</h1>
         <div className="sonrisita">
         <img  className="indice1"src="./img/icons/sonira3.png" alt="flecha"></img>
         </div>
