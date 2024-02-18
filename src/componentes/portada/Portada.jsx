496f845cd175d8fc2986128967641eb9df1a98e7
Alba <albadvg@yahoo.es>
2024-02-18 12:53:20 +0100

    estilos generales repaso

--- src/componentes/portada/Portada.jsx
+++ src/componentes/portada/Portada.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 import "./Portada.scss";
-export default function Portada (){
-    return (
+import {useState} from 'react';
 
+export default function Portada (){
+    const [vw, setVw] = useState(window.innerWidth);
+    window.addEventListener("resize", ()=>setVw(window.innerWidth));
+
+    return (
         <div className="contenedorPortada">
             <h1 className="contenedorPortada__h1">Applergic</h1>
 
             <p>Mi guía alimentaria</p>
-            <Link to={"/bienvenida"}>
-            <img className="imagenPortada" src="./img/logoApplergicFigurasGirocut.png" alt="imagen de portada" />
+            <Link to={"/bienvenida"} className="contenedorPortada-link">
+
+            {vw < 500
+                ? <img className="imagenPortada" src="./img/logoApplergicFigurasGirocut.png" alt="imagen de portada" />
+                : <img className="imagenPortada imagenPortada--desk" src="/img/logoApplergicFigurasGiro@3x.png" alt="imagen portada"/>
+            }
+            
             </Link>
         </div>
     )
